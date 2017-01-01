a0aa05a268fe4c5e3bd2a91464e1346522583a50
Elias Alves <unixelias@gmail.com>
2017-01-01 14:13:18 -0200

    Nova correçã do composer

--- modules/boleto/i-boleto2.php
+++ modules/boleto/i-boleto2.php
@@ -7,17 +7,17 @@ $boleto = new Boletos(Boletos::BOLETOSPHP_BANCOOB);
 $boleto->setData([
     "valor_boleto" => "99,00",
     "data_vencimento" => "01/04/2016",
-    "nosso_numero" => "000001",
+    "nosso_numero" => "23",//passar o índice do boleto
     "valor_boleto" => "25,00",
 	"data_vencimento" => "01/02/2017",
-	"nosso_numero" => 3,
-	"agencia" => "3406",
+	"agencia" => "3046",
 	"conta" => "25368",
 	"conta_dv" => "5",
+	"convenio" => "179682",
 	"carteira" => "1",
 	"identificacao" => "Agência de Viagens BRASILTUR",
 	"cpf_cnpj" => "18.575.104/0001-90",
-	"numero_documento" => "12",
+	"numero_documento" => "TI-009",//por enquanto o mesmo do contrato
 	"data_documento" => "01/04/2016",
 	"data_processamento" => "01/04/2016",
 	"sacado" => "Nome do Sacado",
@@ -27,8 +27,8 @@ $boleto->setData([
 	"demonstrativo1" => "",
 	"demonstrativo2" => "",
 	"demonstrativo3" => "",
-	"instrucoes1" => "",
-	"instrucoes2" => "",
+	"instrucoes1" => "COBRAR MORA/DIAR$ 3,30 APOS O VENCIMENTOPAULO (38) 9893-2164 /  (38) 8815-2940",
+	"instrucoes2" => "REIMPRESSÃO DO BOLETO                  WWW.SICOO.COM.BR",
 	"instrucoes3" => "",
 	"instrucoes4" => "",
 	"quantidade" => "",
@@ -39,8 +39,9 @@ $boleto->setData([
 	"cidade_uf" => "Diamantina/MG",
 	"cedente"=> "PAULO HENRIQUE CRUZ – ME",
 	"contrato"=> "TI-009",
-	"numero_parcela'" => 3,
-    "total_parcelas" => 5
+	"numero_parcela" => "03",
+    "total_parcelas" => "03",
+	"modalidade_cobranca" => "01"
 	
 ]);
 $boleto->setImageBasePath("imagens/");
