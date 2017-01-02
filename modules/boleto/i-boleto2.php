24ed3ba11685c4abf669d5817de2cd6b5262e18a
Elias Alves <unixelias@gmail.com>
2017-01-02 18:18:49 -0200

    Add tutoriais e revisão no layout

--- modules/boleto/i-boleto2.php
+++ modules/boleto/i-boleto2.php
@@ -5,9 +5,14 @@ use GiordanoLima\BoletosPHP\Boletos;
 $boleto = new Boletos(Boletos::BOLETOSPHP_BANCOOB);
 //var_dump($boleto);
 $boleto->setData([
-	"valor_boleto" => "25,00",
+	
+		
+		
+		
+		
+		"valor_boleto" => "25,00",
 	"data_vencimento" => "02/01/2017",
-	"nosso_numero" => "23",//passar o índice do boleto
+	"nosso_numero" => "1600023",//passar o índice do boleto
 	"agencia" => "3046",
 	"conta" => "25368",
 	"conta_dv" => "5",
@@ -20,20 +25,23 @@ $boleto->setData([
 	"data_processamento" => "01/04/2016",
 	"sacado" => "Nome do Sacado",
 	"endereco" => "Rua Silveiro Lessa, 11 Diamantina",
-	"endereco1" => "",
-	"endereco2" => "",
-	"demonstrativo1" => "",
-	"demonstrativo2" => "",
-	"demonstrativo3" => "",
-	"instrucoes1" => "COBRAR MORA/DIA R$ 3,30 APOS O VENCIMENTOPAULO (38) 9893-2164 /  (38) 8815-2940",
-	"instrucoes2" => "REIMPRESSÃO DO BOLETO                  WWW.SICOO.COM.BR",
-	"instrucoes3" => "",
-	"instrucoes4" => "",
-	"quantidade" => "",
-	"valor_unitario" => "",
+	"endereco1" => "Primeira linha endereço cliente",
+	"endereco2" => "Segunda linha endereço cliente",
+	"demonstrativo1" => "Demonstrativo 1 do canhoto - interno/não vai pro banco - informação ao cliente",
+	"demonstrativo2" => "Demonstrativo 2 do canhoto",
+	"demonstrativo3" => "Demonstrativo 3 do canhoto",
+	"instrucoes1" => "COBRAR MORA/DIAR$ 3,30 APOS O VENCIMENTO",
+	"instrucoes2" => "PAULO (38) 9893-2164 /  (38) 8815-2940  ",
+	"instrucoes3" => "REIMPRESSÃO DO BOLETO                   ",
+	"instrucoes4" => "WWW.SICOO.COM.BR @ infromações cliente",
+	"quantidade" => "25,00",
+	"valor_unitario" => "1,0",
+	"valor_desconto_1" => "2,30",
+	"valor_desconto_2" => "4,20",
+	"valor_multa" => "3,14",
 	"aceite" => "N",
-	"especie" => "",
-	"especie_doc" => "",
+	"especie" => "R$",
+	"especie_doc" => "DM",
 	"cidade_uf" => "Diamantina/MG",
 	"cedente"=> "PAULO HENRIQUE CRUZ – ME",
 	"contrato"=> "TI-009",
@@ -44,12 +52,12 @@ $boleto->setData([
 ]);
 $boleto->setImageBasePath("imagens/");
 echo $boleto->render();
-var_dump($boleto);
+//var_dump($boleto);
 $dadosboleto = $boleto->getDadosBoleto();
-echo $dadosboleto['sacado']."<br />";
+//echo $dadosboleto['sacado']."<br />";
 
-foreach ($dadosboleto as $i => $value) {
+/* foreach ($dadosboleto as $i => $value) {
 	echo($dadosboleto[$i]."<br />");
-}
+} */
 
 ?>
